57605e3ec7201a9a9aab6910929cd1384cddbe9c
John <43506685+Coniferish@users.noreply.github.com>
2026-04-21 14:20:23 -0400

    I think this is what I'm supposed to do

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
+import { useState } from 'react'
+
+const todos = [
+    {id: 1, title: "review resources"},
+    {id: 2, title: "take notes"},
+    {id: 3, title: "code out app"},
+  ]
 
 function App() {
-  
+  const [ todoList, setTodoList ] = useState()
 
   return (
     <div>
       <h1>Todo App</h1>
       <TodoForm/>
-      <TodoList/>
+      <TodoList todoList={todos}/>
     </div>
   )
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,5 @@
-export default function TodoList() {
-  const todoList = [
-    {id: 1, title: "review resources"},
-    {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
-  ]
-
+export default function TodoList( {todoList} ) {
+  
   return (
   <>
     <ul>
